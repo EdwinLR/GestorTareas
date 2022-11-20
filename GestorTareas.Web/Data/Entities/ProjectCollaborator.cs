827044bc4c0f407d8e5a37079c1f0eb1df5ad095
AlexBMtz <barroetaalejandro160401@gmail.com>
2022-11-19 22:47:44 -0600

    Actualización de Entidad Project

---
+++ GestorTareas.Web/Data/Entities/ProjectCollaborator.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorTareas.Web.Data.Entities
+{
+    public class ProjectCollaborator:IEntity
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
