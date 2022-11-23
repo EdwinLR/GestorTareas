be0563ff026a3305fd4d8234a61cae494bde559d
AlexBMtz <barroetaalejandro160401@gmail.com>
2022-11-23 15:02:46 -0600

    Modificación de paquetes utilizados en el proyecto web y creación de controlador para API de la entidad Country

---
+++ GestorTareas.Web/Controllers/API/CountriesController.cs
@@ -0,0 +1,40 @@
+﻿using GestorTareas.Web.Data;
+using GestorTareas.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GestorTareas.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+    }
+}
